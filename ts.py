013af6ff0db3ec06d5f62e5194f725f23834cf5e
Abin <abinvs2019@gmail.com>
2023-10-25 22:32:08 +0530

    Final code working for LOCO

--- ts.py
+++ ts.py
@@ -1,5 +1,6 @@
-import os
 import requests
+import os
+import subprocess
 
 def download_file(url, filename):
     response = requests.get(url, stream=True)
@@ -7,34 +8,42 @@ def download_file(url, filename):
         for chunk in response.iter_content(chunk_size=8192):
             file.write(chunk)
 
-def download_ts_video(manifest_url, output_filename):
+def download_video(manifest_url):
+    # Create a directory to store the video segments
+    os.makedirs('segments', exist_ok=True)
+
     # Download the manifest file
     download_file(manifest_url, 'manifest.m3u8')
     
     # Parse the manifest file to get the TS segment URLs
+    base_url = "https://streamcdneu.loco.gg/1540KCGYBH_193b223c-6576-4353-a5e6-28e751725863/1540KCGYBH_193b223c-6576-4353-a5e6-28e751725863_low/"
     with open('manifest.m3u8') as f:
         lines = f.readlines()
-        ts_urls = [line.strip() for line in lines if line.startswith('http')]
-    
-    # Create a list to store the video binary data
-    video_data = []
+        ts_urls = [base_url +line.strip() for line in lines if line.startswith('16')]
     
     # Download the TS segments
     for i, ts_url in enumerate(ts_urls):
-        download_file(ts_url, f'segment_{i}.ts')
-        with open(f'segment_{i}.ts', 'rb') as segment_file:
-            video_data.append(segment_file.read())
-        os.remove(f'segment_{i}.ts')
-    
-    # Combine the binary data into a single video
-    with open(output_filename, 'wb') as out_file:
-        for data in video_data:
-            out_file.write(data)
-    
-    # Clean up temporary files
-    os.remove('manifest.m3u8')
+        download_file(ts_url, f'segments/segment_{i}.ts')
+    concatenate_segments(output_filename)
+    print(f'Downloaded {len(ts_urls)} segments.')
+
+def concatenate_segments(output_filename):
+    segment_files = [f'segments/segment_{i}.ts' for i in range(len(os.listdir('segments')))]
+
+    with open('file_list.txt', 'w') as file_list:
+        for segment_file in segment_files:
+            file_list.write(f"file '{segment_file}'\n")
+
+    cmd = ['ffmpeg', '-f', 'concat', '-safe', '0', '-i', 'file_list.txt', '-c', 'copy', output_filename]
+
+    subprocess.run(cmd, check=True)
+
+
+
 
 if __name__ == "__main__":
-    manifest_url = 'https://streamcdneu.loco.gg/LVQYC9RP2M_fd6371f6-9f8f-4435-9631-a86aeca860cf/LVQYC9RP2M_fd6371f6-9f8f-4435-9631-a86aeca860cf_low/manifest.m3u8'
-    output_filename = 'eagle_output.mp4'
-    download_ts_video(manifest_url, output_filename)
+    manifest_url = "https://streamcdneu.loco.gg/1540KCGYBH_193b223c-6576-4353-a5e6-28e751725863/1540KCGYBH_193b223c-6576-4353-a5e6-28e751725863_hi/video.m3u8"
+    output_filename = 'file_out.mp4'
+    
+    download_video(manifest_url)
+
